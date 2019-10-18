bddbd6351b91fe2885d37a235dd55cc207471367
Solomon Hawk <solomon.hawk@viget.com>
2019-10-18 17:12:56 -0400

    Final touches

--- languages/elixir_sol/lib/unscramble.ex
+++ languages/elixir_sol/lib/unscramble.ex
@@ -2,14 +2,31 @@ defmodule Unscramble do
   @moduledoc """
   Documentation for Unscramble.
   """
+
+  @dict_path "/usr/share/dict/words"
+
   def parse(letters) do
-    input = make_hash(letters)
-    lookup = prepare_dictionary()
+    input_hash = make_hash(letters)
+
+    dictionary_stream()
+    |> Stream.map(fn word ->
+      String.trim(word)
+    end)
+    |> Stream.filter(fn word ->
+      suitable_candidate(word, letters) && hashes_are_equal(make_hash(word), input_hash)
+    end)
+    |> Enum.to_list()
+  end
+
+  def dictionary_stream() do
+    File.stream!(@dict_path)
   end
 
   def make_hash(letters) do
     make_hash(String.split(letters, "", trim: true), %{})
   end
+
+  def make_hash(["\n"|tail], acc), do: make_hash(tail, acc)
 
   def make_hash([head|tail], acc) do
     cond do
@@ -22,8 +39,10 @@ defmodule Unscramble do
 
   def make_hash([], acc), do: acc
 
-  def prepare_dictionary() do
-    File.read!("/usr/share/dict/words")
-    |> String.split("\n")
+  def suitable_candidate(word, letters) do
+    String.length(word) == String.length(letters) && String.contains?(letters, String.first(word))
   end
+
+  def hashes_are_equal(hash, hash), do: true
+  def hashes_are_equal(_a, _b), do: false
 end
