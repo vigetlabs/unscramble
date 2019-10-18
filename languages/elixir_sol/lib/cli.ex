224caaac65a91db2d43ade93bf3f5d13f1d2dbfd
Solomon Hawk <solomon.hawk@viget.com>
2019-10-18 15:53:15 -0400

    Add make_hash, prepare_dictionary

--- languages/elixir_sol/lib/cli.ex
+++ languages/elixir_sol/lib/cli.ex
@@ -2,7 +2,7 @@ defmodule Unscramble.CLI do
   @moduledoc """
   Documentation for Unscramble.
   """
-  def main(_args) do
-    IO.puts("Hello, world!")
+  def main([scrambled_word | _]) do
+    Unscramble.parse(scrambled_word)
   end
 end
--- languages/elixir_sol/lib/unscramble.ex
+++ languages/elixir_sol/lib/unscramble.ex
@@ -2,7 +2,28 @@ defmodule Unscramble do
   @moduledoc """
   Documentation for Unscramble.
   """
-  def hello do
-    :world
+  def parse(letters) do
+    input = make_hash(letters)
+    lookup = prepare_dictionary()
+  end
+
+  def make_hash(letters) do
+    make_hash(String.split(letters, "", trim: true), %{})
+  end
+
+  def make_hash([head|tail], acc) do
+    cond do
+      Map.has_key?(acc, head) ->
+        make_hash(tail, Map.put(acc, head, acc[head] + 1))
+      true ->
+        make_hash(tail, Map.put(acc, head, 1))
+    end
+  end
+
+  def make_hash([], acc), do: acc
+
+  def prepare_dictionary() do
+    File.read!("/usr/share/dict/words")
+    |> String.split("\n")
   end
 end
