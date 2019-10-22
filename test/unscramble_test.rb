4b9b509917376cbc985b8dd773d1487e2d17b1f4
Patrick Reagan <github@the-reagans.com>
2019-10-22 10:51:54 -0600

    Handle variances in dictionaries in test suite

--- test/unscramble_test.rb
+++ test/unscramble_test.rb
@@ -2,22 +2,25 @@ require 'minitest/autorun'
 
 class UnscrambleTest < MiniTest::Test
   JUMBLES = {
-    'tmorcpue'  => 'computer',
-    'crootd'    => 'doctor',
-    'salos'     => 'lasso',
-    'gfroo'     => 'forgo',
-    'cluen'     => 'uncle',
-    'rubwor'    => 'burrow',
-    'ittcek'    => 'ticket',
-    'nitmyu'    => 'mutiny',
-    'cuthh'     => 'hutch',
-    'rptiem'    => 'permit',
-    'fryrul'    => 'flurry'
+    'tmorcpue'  => ['computer'],
+    'crootd'    => ['doctor'],
+    'salos'     => ['lasso', 'ossal'],
+    'gfroo'     => ['forgo'],
+    'cluen'     => ['uncle'],
+    'rubwor'    => ['burrow'],
+    'ittcek'    => ['ticket'],
+    'nitmyu'    => ['mutiny', 'munity'],
+    'cuthh'     => ['hutch'],
+    'rptiem'    => ['permit'],
+    'fryrul'    => ['flurry']
   }
 
-  JUMBLES.each do |input, output|
-    define_method "test_#{input}_unscrambles_to_#{output}" do
-      assert_equal output, `#{ENV["LANGUAGE_PATH"]}/unscramble #{input}`.strip
+  JUMBLES.each do |input, allowed_values|
+    define_method "test_#{input}_unscrambles_to_#{allowed_values.first}" do
+      output  = `#{ENV["LANGUAGE_PATH"]}/unscramble #{input}`.strip
+      message = "Expected '#{input}' to equal '#{allowed_values.first}' or another allowed value"
+
+      assert(allowed_values.include?(output), message)
     end
   end
 end
