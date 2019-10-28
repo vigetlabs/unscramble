712c608bc16dec9d01d1530fa5eda03d4c663aa3
Joe Jackson <cpmhjoe@gmail.com>
2019-10-28 15:59:43 -0400

    Reuse a buffer

--- languages/Rust/src/main.rs
+++ languages/Rust/src/main.rs
@@ -12,19 +12,26 @@ fn main() {
 
 fn get_matches(word: &str) -> io::Result<Vec<String>> {
   let file = File::open("/usr/share/dict/words")?;
-  let reader = BufReader::new(file);
+  let mut reader = BufReader::new(file);
   let mut vec: Vec<String> = Vec::new();
   let sorted_word = sort_chars(word);
+  let mut buff = String::new();
 
-  for line in reader.lines() {
-    let line = line?;
-    let mut line_chars = line.chars();
-    if line.len() == sorted_word.len() && line_chars.any(|x| x == sorted_word[0]) {
-      let sorted_line = sort_chars(&line);
+  while let Ok(done) = reader.read_line(&mut buff) {
+    if done == 0 {
+      break;
+    }
+    let trimmed = buff.trim_end();
+    let mut line_chars = trimmed.chars();
+    if trimmed.len() == sorted_word.len() && line_chars.any(|x| x == sorted_word[0]) {
+      let sorted_line = sort_chars(&trimmed);
       if sorted_line == sorted_word {
-        vec.push(line.clone());
+        vec.push(trimmed.to_string());
+        break;
       }
     }
+
+    buff.clear();
   }
 
   Ok(vec)
